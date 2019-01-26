f0d106adbf5cfc536a7146ea316cd928de89d923
shin-kinoshita <kinoshita116@gmail.com>
2019-01-26 17:35:56 +0900

    add env limitation for executing spolicy replace-schema ddl

--- dbflute_maihamadb/playsql/replace-schema-90-spolicyworld.sql
+++ dbflute_maihamadb/playsql/replace-schema-90-spolicyworld.sql
@@ -1,2 +1,3 @@
+-- #df:checkEnv(spolicy)#
 alter table PRODUCT add PRODUCT_ONSALES_FLG integer not null after PRODUCT_STATUS_CODE;
 alter table PRODUCT add ONSALES_APPLICABLE_SERVICE_RANK char(3) after PRODUCT_ONSALES_FLG;
