329100a7202eca7e2093b7ff30595fb206247044
jflute <dbflute@gmail.com>
2019-03-31 17:46:26 +0900

    add cp -R ./extplates/om $DBFLUTE_HOME/templates

--- dbflute_maihamadb/manage.sh
+++ dbflute_maihamadb/manage.sh
@@ -6,6 +6,9 @@ cd `dirname $0`
 FIRST_ARG=$1
 SECOND_ARG=$2
 
+echo "...Overriding DBFlute Engine templates for application customization"
+cp -R ./extplates/om $DBFLUTE_HOME/templates
+
 sh $DBFLUTE_HOME/etc/cmd/_df-manage.sh $MY_PROPERTIES_PATH $FIRST_ARG $SECOND_ARG
 taskReturnCode=$?
 
