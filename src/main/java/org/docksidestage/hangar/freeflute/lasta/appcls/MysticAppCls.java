5801306c58a5987e1c6019242f716a6cbee512ec
jflute <jflute@jflute-nyabara>
2021-06-12 19:00:29 +0900

    use DBFlute snapshot

--- src/main/java/org/docksidestage/hangar/freeflute/lasta/appcls/MysticAppCls.java
+++ src/main/java/org/docksidestage/hangar/freeflute/lasta/appcls/MysticAppCls.java
@@ -109,7 +109,6 @@ public interface MysticAppCls extends Classification {
          * Get the classification by the code. (CaseInsensitive)
          * @param code The value of code, which is case-insensitive. (NullAllowed: if null, returns null)
          * @return The instance of the corresponding classification to the code. (NullAllowed: if not found, returns null)
-         * @deprecated use of()
          */
         public static SearchMemberStatus codeOf(Object code) {
             if (code == null) { return null; }
@@ -191,9 +190,185 @@ public interface MysticAppCls extends Classification {
         @Override public String toString() { return code(); }
     }
 
+    /**
+     * MemberStatus for search condition
+     */
+    public enum TestDeprecatedClsStatus implements MysticAppCls {
+        /** Formalized: as formal member, allowed to use all service */
+        Formalized("FML", "Formalized", emptyStrings())
+        ,
+        /** Withdrawal: withdrawal is fixed, not allowed to use service */
+        Withdrawal("WDL", "Withdrawal", emptyStrings())
+        ,
+        /** Provisional: first status after entry, allowed to use only part of service */
+        Provisional("PRV", "Provisional", emptyStrings())
+        ,
+        /** All Statuses: without status filter */
+        All("ALL", "All Statuses", emptyStrings())
+        ;
+        private static final Map<String, TestDeprecatedClsStatus> _codeClsMap = new HashMap<String, TestDeprecatedClsStatus>();
+        private static final Map<String, TestDeprecatedClsStatus> _nameClsMap = new HashMap<String, TestDeprecatedClsStatus>();
+        static {
+            for (TestDeprecatedClsStatus value : values()) {
+                _codeClsMap.put(value.code().toLowerCase(), value);
+                for (String sister : value.sisterSet()) { _codeClsMap.put(sister.toLowerCase(), value); }
+            }
+        }
+        private static final Map<String, Map<String, Object>> _subItemMapMap = new HashMap<String, Map<String, Object>>();
+        static {
+            {
+                Map<String, Object> subItemMap = new HashMap<String, Object>();
+                subItemMap.put("key1", "1");
+                subItemMap.put("key2", "as formal member, allowed to use all service");
+                _subItemMapMap.put(Formalized.code(), Collections.unmodifiableMap(subItemMap));
+            }
+            {
+                Map<String, Object> subItemMap = new HashMap<String, Object>();
+                subItemMap.put("key1", "2");
+                subItemMap.put("key2", "withdrawal is fixed, not allowed to use service");
+                _subItemMapMap.put(Withdrawal.code(), Collections.unmodifiableMap(subItemMap));
+            }
+            {
+                Map<String, Object> subItemMap = new HashMap<String, Object>();
+                subItemMap.put("key1", "3");
+                subItemMap.put("key2", "first status after entry, allowed to use only part of service");
+                _subItemMapMap.put(Provisional.code(), Collections.unmodifiableMap(subItemMap));
+            }
+            {
+                Map<String, Object> subItemMap = new HashMap<String, Object>();
+                _subItemMapMap.put(All.code(), Collections.unmodifiableMap(subItemMap));
+            }
+        }
+        private String _code; private String _alias; private Set<String> _sisterSet;
+        private TestDeprecatedClsStatus(String code, String alias, String[] sisters)
+        { _code = code; _alias = alias; _sisterSet = Collections.unmodifiableSet(new LinkedHashSet<String>(Arrays.asList(sisters))); }
+        public String code() { return _code; } public String alias() { return _alias; }
+        public Set<String> sisterSet() { return _sisterSet; }
+        public Map<String, Object> subItemMap() { return _subItemMapMap.get(code()); }
+        public ClassificationMeta meta() { return MysticAppCls.DefMeta.TestDeprecatedClsStatus; }
+
+        public boolean inGroup(String groupName) {
+            return false;
+        }
+
+        /**
+         * Get the classification of the code. (CaseInsensitive)
+         * @param code The value of code, which is case-insensitive. (NullAllowed: if null, returns empty)
+         * @return The optional classification corresponding to the code. (NotNull, EmptyAllowed: if not found, returns empty)
+         */
+        public static OptionalThing<TestDeprecatedClsStatus> of(Object code) {
+            if (code == null) { return OptionalThing.ofNullable(null, () -> { throw new ClassificationNotFoundException("null code specified"); }); }
+            if (code instanceof TestDeprecatedClsStatus) { return OptionalThing.of((TestDeprecatedClsStatus)code); }
+            if (code instanceof OptionalThing<?>) { return of(((OptionalThing<?>)code).orElse(null)); }
+            return OptionalThing.ofNullable(_codeClsMap.get(code.toString().toLowerCase()), () ->{
+                throw new ClassificationNotFoundException("Unknown classification code: " + code);
+            });
+        }
+
+        /**
+         * Find the classification by the name. (CaseInsensitive)
+         * @param name The string of name, which is case-insensitive. (NotNull)
+         * @return The optional classification corresponding to the name. (NotNull, EmptyAllowed: if not found, returns empty)
+         */
+        public static OptionalThing<TestDeprecatedClsStatus> byName(String name) {
+            if (name == null) { throw new IllegalArgumentException("The argument 'name' should not be null."); }
+            return OptionalThing.ofNullable(_nameClsMap.get(name.toLowerCase()), () ->{
+                throw new ClassificationNotFoundException("Unknown classification name: " + name);
+            });
+        }
+
+        /**
+         * <span style="color: #AD4747; font-size: 120%">Old style so use of(code).</span> <br>
+         * Get the classification by the code. (CaseInsensitive)
+         * @param code The value of code, which is case-insensitive. (NullAllowed: if null, returns null)
+         * @return The instance of the corresponding classification to the code. (NullAllowed: if not found, returns null)
+         */
+        public static TestDeprecatedClsStatus codeOf(Object code) {
+            if (code == null) { return null; }
+            if (code instanceof TestDeprecatedClsStatus) { return (TestDeprecatedClsStatus)code; }
+            return _codeClsMap.get(code.toString().toLowerCase());
+        }
+
+        /**
+         * <span style="color: #AD4747; font-size: 120%">Old style so use byName(name).</span> <br>
+         * Get the classification by the name (also called 'value' in ENUM world).
+         * @param name The string of name, which is case-sensitive. (NullAllowed: if null, returns null)
+         * @return The instance of the corresponding classification to the name. (NullAllowed: if not found, returns null)
+         */
+        public static TestDeprecatedClsStatus nameOf(String name) {
+            if (name == null) { return null; }
+            try { return valueOf(name); } catch (RuntimeException ignored) { return null; }
+        }
+
+        /**
+         * Get the list of all classification elements. (returns new copied list)
+         * @return The snapshot list of all classification elements. (NotNull)
+         */
+        public static List<TestDeprecatedClsStatus> listAll() {
+            return new ArrayList<TestDeprecatedClsStatus>(Arrays.asList(values()));
+        }
+
+        /**
+         * Get the list of classification elements in the specified group. (returns new copied list) <br>
+         * @param groupName The string of group name, which is case-insensitive. (NotNull)
+         * @return The snapshot list of classification elements in the group. (NotNull, EmptyAllowed: if not found, throws exception)
+         */
+        public static List<TestDeprecatedClsStatus> listByGroup(String groupName) {
+            if (groupName == null) { throw new IllegalArgumentException("The argument 'groupName' should not be null."); }
+            throw new ClassificationNotFoundException("Unknown classification group: TestDeprecatedClsStatus." + groupName);
+        }
+
+        /**
+         * Get the list of classification elements corresponding to the specified codes. (returns new copied list) <br>
+         * @param codeList The list of plain code, which is case-insensitive. (NotNull)
+         * @return The snapshot list of classification elements in the code list. (NotNull, EmptyAllowed: when empty specified)
+         */
+        public static List<TestDeprecatedClsStatus> listOf(Collection<String> codeList) {
+            if (codeList == null) { throw new IllegalArgumentException("The argument 'codeList' should not be null."); }
+            List<TestDeprecatedClsStatus> clsList = new ArrayList<TestDeprecatedClsStatus>(codeList.size());
+            for (String code : codeList) { clsList.add(of(code).get()); }
+            return clsList;
+        }
+
+        /**
+         * Get the list of classification elements in the specified group. (returns new copied list) <br>
+         * @param groupName The string of group name, which is case-sensitive. (NullAllowed: if null, returns empty list)
+         * @return The snapshot list of classification elements in the group. (NotNull, EmptyAllowed: if the group is not found)
+         */
+        public static List<TestDeprecatedClsStatus> groupOf(String groupName) {
+            return new ArrayList<TestDeprecatedClsStatus>(4);
+        }
+
+        /**
+         * @param dbCls The DB classification to find. (NullAllowed: if null, returns empty)
+         * @return The the app classification corresponding to the DB classification. (NotNull, EmptyAllowed: when null specified, not found)
+         */
+        public static OptionalThing<TestDeprecatedClsStatus> fromDBCls(CDef.MemberStatus dbCls) {
+            String dbCode = dbCls != null ? dbCls.code() : null;
+            return OptionalThing.ofNullable(codeOf(dbCode), () -> {
+                throw new IllegalStateException("Cannot convert CDef.MemberStatus to TestDeprecatedClsStatus by the DB code: " + dbCode);
+            });
+        }
+
+        /**
+         * @return The DB classification corresponding to the app classification. (NotNull, EmptyAllowed: when no-related to DB)
+         */
+        public OptionalThing<CDef.MemberStatus> toDBCls() {
+            String appCode = code();
+            return OptionalThing.ofNullable(CDef.MemberStatus.codeOf(appCode), () -> {
+                throw new IllegalStateException("Cannot convert TestDeprecatedClsStatus to MemberStatus by the app code: " + appCode);
+            });
+        }
+
+        @Override public String toString() { return code(); }
+    }
+
     public enum DefMeta implements ClassificationMeta {
         /** MemberStatus for search condition */
         SearchMemberStatus
+        ,
+        /** MemberStatus for search condition */
+        TestDeprecatedClsStatus
         ;
         public String classificationName() {
             return name(); // same as definition name
@@ -201,41 +376,49 @@ public interface MysticAppCls extends Classification {
 
         public OptionalThing<? extends Classification> of(Object code) {
             if (SearchMemberStatus.name().equals(name())) { return MysticAppCls.SearchMemberStatus.of(code); }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return MysticAppCls.TestDeprecatedClsStatus.of(code); }
             throw new IllegalStateException("Unknown definition: " + this); // basically unreachable
         }
 
         public OptionalThing<? extends Classification> byName(String name) {
             if (SearchMemberStatus.name().equals(name())) { return MysticAppCls.SearchMemberStatus.byName(name); }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return MysticAppCls.TestDeprecatedClsStatus.byName(name); }
             throw new IllegalStateException("Unknown definition: " + this); // basically unreachable
         }
 
-        public Classification codeOf(Object code) { // null if not found, old style so use classificationOf(code)
+        public Classification codeOf(Object code) { // null if not found, old style so use of(code)
             if (SearchMemberStatus.name().equals(name())) { return MysticAppCls.SearchMemberStatus.codeOf(code); }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return MysticAppCls.TestDeprecatedClsStatus.codeOf(code); }
             throw new IllegalStateException("Unknown definition: " + this); // basically unreachable
         }
 
-        public Classification nameOf(String name) { // null if not found, old style so use classificationByName(name)
+        public Classification nameOf(String name) { // null if not found, old style so use byName(name)
             if (SearchMemberStatus.name().equals(name())) { return MysticAppCls.SearchMemberStatus.valueOf(name); }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return MysticAppCls.TestDeprecatedClsStatus.valueOf(name); }
             throw new IllegalStateException("Unknown definition: " + this); // basically unreachable
         }
 
         public List<Classification> listAll() {
             if (SearchMemberStatus.name().equals(name())) { return toClsList(MysticAppCls.SearchMemberStatus.listAll()); }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return toClsList(MysticAppCls.TestDeprecatedClsStatus.listAll()); }
             throw new IllegalStateException("Unknown definition: " + this); // basically unreachable
         }
 
         public List<Classification> listByGroup(String groupName) { // exception if not found
             if (SearchMemberStatus.name().equals(name())) { return toClsList(MysticAppCls.SearchMemberStatus.listByGroup(groupName)); }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return toClsList(MysticAppCls.TestDeprecatedClsStatus.listByGroup(groupName)); }
             throw new IllegalStateException("Unknown groupName: " + groupName + ", " + this); // basically unreachable
         }
 
         public List<Classification> listOf(Collection<String> codeList) {
             if (SearchMemberStatus.name().equals(name())) { return toClsList(MysticAppCls.SearchMemberStatus.listOf(codeList)); }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return toClsList(MysticAppCls.TestDeprecatedClsStatus.listOf(codeList)); }
             throw new IllegalStateException("Unknown definition: " + this); // basically unreachable
         }
 
         public List<Classification> groupOf(String groupName) { // old style
             if (SearchMemberStatus.name().equals(name())) { return toClsList(MysticAppCls.SearchMemberStatus.groupOf(groupName)); }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return toClsList(MysticAppCls.TestDeprecatedClsStatus.groupOf(groupName)); }
             throw new IllegalStateException("Unknown definition: " + this); // basically unreachable
         }
 
@@ -246,17 +429,20 @@ public interface MysticAppCls extends Classification {
 
         public ClassificationCodeType codeType() {
             if (SearchMemberStatus.name().equals(name())) { return ClassificationCodeType.String; }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return ClassificationCodeType.String; }
             return ClassificationCodeType.String; // as default
         }
 
         public ClassificationUndefinedHandlingType undefinedHandlingType() {
             if (SearchMemberStatus.name().equals(name())) { return ClassificationUndefinedHandlingType.LOGGING; }
+            if (TestDeprecatedClsStatus.name().equals(name())) { return ClassificationUndefinedHandlingType.LOGGING; }
             return ClassificationUndefinedHandlingType.LOGGING; // as default
         }
 
         public static OptionalThing<MysticAppCls.DefMeta> find(String classificationName) { // instead of valueOf()
             if (classificationName == null) { throw new IllegalArgumentException("The argument 'classificationName' should not be null."); }
             if (SearchMemberStatus.name().equalsIgnoreCase(classificationName)) { return OptionalThing.of(MysticAppCls.DefMeta.SearchMemberStatus); }
+            if (TestDeprecatedClsStatus.name().equalsIgnoreCase(classificationName)) { return OptionalThing.of(MysticAppCls.DefMeta.TestDeprecatedClsStatus); }
             return OptionalThing.ofNullable(null, () -> {
                 throw new ClassificationNotFoundException("Unknown classification: " + classificationName);
             });
@@ -265,6 +451,7 @@ public interface MysticAppCls extends Classification {
         public static MysticAppCls.DefMeta meta(String classificationName) { // old style so use find(name)
             if (classificationName == null) { throw new IllegalArgumentException("The argument 'classificationName' should not be null."); }
             if (SearchMemberStatus.name().equalsIgnoreCase(classificationName)) { return MysticAppCls.DefMeta.SearchMemberStatus; }
+            if (TestDeprecatedClsStatus.name().equalsIgnoreCase(classificationName)) { return MysticAppCls.DefMeta.TestDeprecatedClsStatus; }
             throw new IllegalStateException("Unknown classification: " + classificationName);
         }
 
