2e1fbd763ee1cc71bd61a5135d16c49a7a72b000
jflute <dbflute@gmail.com>
2023-10-18 18:09:55 +0900

    upgrade DBFlute to 1.2.7

--- src/main/java/org/docksidestage/hangar/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
+++ src/main/java/org/docksidestage/hangar/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
@@ -78,7 +78,7 @@ public class VendorIdentityOnlyDbm extends AbstractDBMeta {
     // ===================================================================================
     //                                                                         Column Info
     //                                                                         ===========
-    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, Long.class, "identityOnlyId", null, true, true, true, "BIGINT", 19, 0, null, "NEXT VALUE FOR \"PUBLIC\".\"SYSTEM_SEQUENCE_0E90433D_5D3D_43A6_871B_D8BCB3BB426A\"", false, null, null, null, null, null, false);
+    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, Long.class, "identityOnlyId", null, true, true, true, "BIGINT", 19, 0, null, "NEXT VALUE FOR \"PUBLIC\".\"SYSTEM_SEQUENCE_FF529103_9205_40A5_A8E9_8E3250EC3A52\"", false, null, null, null, null, null, false);
 
     /**
      * IDENTITY_ONLY_ID: {PK, ID, NotNull, BIGINT(19)}
--- src/test/java/org/docksidestage/hangar/dbflute/friends/gson/GuiceFireTest.java
+++ src/test/java/org/docksidestage/hangar/dbflute/friends/gson/GuiceFireTest.java
@@ -1,16 +1,24 @@
 package org.docksidestage.hangar.dbflute.friends.gson;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.docksidestage.hangar.dbflute.friends.gson.GuiceFireTest.Maihama.Land;
 import org.docksidestage.hangar.dbflute.friends.gson.GuiceFireTest.Maihama.Sea;
+import org.docksidestage.hangar.dbflute.friends.gson.GuiceFireTest.Maihama.Stage;
 import org.docksidestage.hangar.unit.UnitContainerTestCase;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
 import io.gsonfire.GsonFireBuilder;
+import io.gsonfire.TypeSelector;
 
 /**
+ * Does the gson-fire work with latest Gson? <br>
+ * (Supposing LastaFlute dependency conflict)
  * @author jflute
  */
 public class GuiceFireTest extends UnitContainerTestCase {
@@ -23,7 +31,7 @@ public class GuiceFireTest extends UnitContainerTestCase {
     //                                                ------
     public void test_plain_fromJson_object_seaOnly() {
         // ## Arrange ##
-        Gson gson = new GsonFireBuilder().createGson();
+        Gson gson = createPlainFire().createGson();
         String json = "{\"sea\" : { \"harborName\" : \"medi\", \"nameLength\" : 3 }}";
 
         // ## Act ##
@@ -39,12 +47,66 @@ public class GuiceFireTest extends UnitContainerTestCase {
         log(gson.toJson(maihama)); // visual check
     }
 
+    public void test_plain_toJson_object_full() {
+        // ## Arrange ##
+        Gson gson = createPlainFire().createGson();
+        Maihama maihama = new Maihama();
+        {
+            Sea sea = new Sea();
+            sea.harborName = "mystic";
+            sea.nameLength = 3;
+            List<Stage> stageList = new ArrayList<Stage>();
+            {
+                Stage stage = new Stage();
+                stage.stageName = "dockside";
+                stage.beginingDate = LocalDate.of(2010, 10, 28);
+                stageList.add(stage);
+            }
+            {
+                Stage stage = new Stage();
+                stage.stageName = "hangar";
+                stage.beginingDate = LocalDate.of(2016, 3, 5);
+                stageList.add(stage);
+            }
+            sea.stageList = stageList;
+            maihama.sea = sea;
+        }
+        {
+            Land land = new Land();
+            land.castleName = "cinderella";
+            land.nameLength = 4;
+            List<Stage> stageList = new ArrayList<Stage>();
+            {
+                Stage stage = new Stage();
+                stage.stageName = "showbase";
+                stage.beginingDate = LocalDate.of(2023, 8, 25);
+                stageList.add(stage);
+            }
+            {
+                Stage stage = new Stage();
+                stage.stageName = "orleans";
+                stage.beginingDate = LocalDate.of(2020, 1, 9);
+                stageList.add(stage);
+            }
+            land.stageList = stageList;
+            maihama.land = land;
+        }
+
+        // ## Act ##
+        String json = gson.toJson(maihama);
+
+        // ## Assert ##
+        log(json);
+        Maihama reversed = gson.fromJson(json, Maihama.class);
+        log(reversed); // visual check, stageList cannot be reverted
+    }
+
     // -----------------------------------------------------
     //                                            Simple Map
     //                                            ----------
     public void test_plain_fromJson_map() {
         // ## Arrange ##
-        Gson gson = new GsonFireBuilder().createGson();
+        Gson gson = createPlainFire().createGson();
         String json = "{\"sea\" : \"mystic\"}";
 
         // ## Act ##
@@ -57,8 +119,44 @@ public class GuiceFireTest extends UnitContainerTestCase {
     }
 
     // ===================================================================================
+    //                                                                               Typed
+    //                                                                               =====
+    public void test_typed_fromJson_object_seaOnly() {
+        // ## Arrange ##
+        Gson gson = createPlainFire().createGson();
+        String json = "{\"sea\" : { \"harborName\" : \"medi\", \"nameLength\" : 3 }}";
+
+        // ## Act ##
+        Maihama maihama = gson.fromJson(json, Maihama.class);
+
+        // ## Assert ##
+        log(maihama);
+        Sea sea = maihama.sea;
+        assertEquals("medi", sea.harborName);
+        assertEquals(3, sea.nameLength);
+        Land land = maihama.land;
+        assertNull(land);
+        log(gson.toJson(maihama)); // visual check
+    }
+
+    // ===================================================================================
     //                                                                        Small Helper
     //                                                                        ============
+    private GsonFireBuilder createPlainFire() {
+        return new GsonFireBuilder();
+    }
+
+    private GsonFireBuilder createTypedFire() {
+        GsonFireBuilder builder = new GsonFireBuilder();
+        builder.registerTypeSelector(LocalDate.class, new TypeSelector<LocalDate>() {
+            public Class<? extends LocalDate> getClassForElement(JsonElement readElement) {
+                // TODO jflute xxxxxxx (2023/08/25)
+                return null;
+            }
+        });
+        return builder;
+    }
+    
     public static class Maihama {
 
         public Sea sea;
@@ -67,6 +165,7 @@ public class GuiceFireTest extends UnitContainerTestCase {
 
             public String harborName;
             public Integer nameLength;
+            public List<Stage> stageList;
 
             @Override
             public String toString() {
@@ -80,11 +179,21 @@ public class GuiceFireTest extends UnitContainerTestCase {
 
             public String castleName;
             public Integer nameLength;
+            public List<Stage> stageList;
 
             @Override
             public String toString() {
                 return "{" + castleName + ", " + nameLength + "}";
             }
+        }
+
+        /**
+         * @author jflute
+         */
+        public static class Stage {
+
+            public String stageName;
+            public LocalDate beginingDate;
         }
 
         @Override
